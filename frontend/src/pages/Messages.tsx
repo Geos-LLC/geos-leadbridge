566e2e8100ab2453107a62ebba245ae8504f7738
goscha01 <sayapingeorge@gmail.com>
2026-02-03 16:04:17 -0500

    Fix TypeScript return type inference in SSE handler

--- frontend/src/pages/Messages.tsx
+++ frontend/src/pages/Messages.tsx
@@ -205,7 +205,7 @@ export function Messages() {
         if (data.type === 'lead.created') {
           console.log('[Messages] New lead received via SSE:', data.lead);
           // Add the new lead to the beginning of the list
-          setLeads((prevLeads: Lead[]) => [data.lead, ...prevLeads]);
+          setLeads((prevLeads: Lead[]): Lead[] => [data.lead as Lead, ...prevLeads]);
         }
       } catch (err) {
         console.error('[Messages] Error parsing SSE event:', err);
